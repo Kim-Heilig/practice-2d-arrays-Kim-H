46d67764bac9c3430a9b3f4aa24cd1e243616261
Kim Heilig <kimmarieheilig@gmail.com>
2025-07-20 07:32:16 -0400

    exercise complete

--- building-2D-arrays.js
+++ building-2D-arrays.js
@@ -1,1 +1,20 @@
+// Tasks Challenge 1: Student Scores You need to organize scores for three students, each of whom has taken three tests. Create a 2D array named allScores where each inner array contains three test scores for a single student. 1. Write the code to create the allScores array with three inner arrays. // 2. Each inner array should contain three numeric scores of your choice.
 
+let allScores = [[80, 90, 93], [74, 75, 76], [88, 98, 92]];
+
+
+// Challenge 2: Build a Seating Grid You want to model three rows of seats in a theater with four seats per row. Create a 2D array called theaterSeating that has an outer array of three inner arrays, each inner array representing a row of four seat labels. (For instance, seat labels could be "A1", "A2", etc.)
+
+// 1. Use nested array literals to define the structure.
+// 2. Ensure each inner array has exactly four seat labels.
+
+let seatingGrid = [["E8", "E9", "E10", "E11"], ["F8", "F9", "F10", "F11"], ["G8", "G9", "G10", "G11"]];
+
+
+// Challenge 3: Form a Multi-Dimensional Contact List Create a 2D 
+// array named contactsList where each inner array represents one contact’s info. For now, store three pieces of information in each inner array, such as ["Name", "Email", "Phone"]. Create two of these inner arrays in contactsList. 
+
+// 1. Define the outer array contactsList.
+// 2. Insert two inner arrays, each with three strings representing a contact’s details. 1
+
+let contactList =[["Kim", "kim@myemail.com", "609-555-5555"], ["Mary", "mary@marysemail.com", "215-444-4444"]];
